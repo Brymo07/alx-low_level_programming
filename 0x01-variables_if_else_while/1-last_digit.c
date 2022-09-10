08783ff75c39f07e368e8c77a212506630beb177
Brymo07 <felixowusu479@gmail.com>
2022-09-09 19:37:54 -0700

    C loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
@@ -9,8 +7,7 @@
  *  *main - Entry point
  *
  *   *Return: Always 0 (Success)
- *
- *    */
+ */
 
 int main(void)
 {
