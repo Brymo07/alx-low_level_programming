65ff2a517e8ee69da3075046089a4cb5240cec32
Brymo07 <felixowusu479@gmail.com>
2022-09-16 03:15:56 +0000

    more functions and loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,46 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - Prints the n times table
- *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
+ * main - prints largest prime factor.
+ * Return: Always 0.
  */
-void print_times_table(int n)
-{
-	int a, b, op;
 
-	if (n >= 0 && n <= 15)
+int main(void)
+{
+	long int n, fp;
+
+	n = 612852475143;
+	for (fp =2; fp <= n; fp++)
 	{
-		for (a = 0; a <= n; a++)
+		if (n % fp == 0)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op * 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) + 48) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
+			n /= fp;
+			fp--;
 		}
 	}
+	printf("%ld\n", fp);
+	return (0);
 }
